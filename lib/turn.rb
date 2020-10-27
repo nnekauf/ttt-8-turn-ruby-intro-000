46edfd2adb6714ae2150d99f1f9779de5e8b3236
industrious-chip-8083 <ufumakanneka@gmail.com>
2020-10-27 22:50:34 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -50,9 +50,7 @@ board = [" "," "," "," "," "," "," "," "," "]
     num = gets.chomp
     index=input_to_index(num)
     if valid_move?(board, index) == false
-      puts "Please enter 1-9:"
-      num = gets.chomp
-      index=input_to_index(num)
+      turn(board)
     end
   end 
   
